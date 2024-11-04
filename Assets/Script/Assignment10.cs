78625b0cf379e9947a495007c9327d6885f2166f
Omar Samhan <omarabusamhan@gmail.com>
2024-11-04 14:34:14 +0200

    first commit

---
+++ Assets/Script/Assignment10.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assignment10 : MonoBehaviour
+{
+    void Start()
+    {
+        for(int i = 5; i == 5; i++)
+        {
+            for(int j = 1; j<= 5; j++)
+            {
+                int avg = i * j;
+                Debug.Log(i + " * " + j + " = " + avg);
+            }
+        }
+    }
+
+
+}
